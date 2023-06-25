9da58c30c646412af3a5952970d7095985d7b87c
hadjidoro <>
2023-06-25 16:34:40 +0000

    menu base on role testing

--- src/app/components/layouts/partials/sidebar/sidebar.component.ts
+++ src/app/components/layouts/partials/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {KeycloakService} from "keycloak-angular";
 
 @Component({
   selector: 'app-sidebar',
@@ -11,6 +12,8 @@ export class SidebarComponent {
 
     @Output()
     sidebarClosed = new EventEmitter
+    constructor(public keycloakService: KeycloakService) {
+    }
 
     toggleSidebar() {
         this.showMobileSideBar = !this.showMobileSideBar
