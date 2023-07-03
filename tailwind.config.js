4885fd417aa836a45980a1c9f4193d999c2693cf
hadjidoro <>
2023-07-03 15:18:43 +0000

    add max-w-* class to safelist since it’s impossible to scan wildcard value because it is passed on component creation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-      "./src/**/*.{html,ts}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-      require('@tailwindcss/forms'),
-  ],
+    content: [
+        "./src/**/*.{html,ts}",
+    ],
+    safelist: [
+        {
+            pattern: /max-w-(xs|md|lg|xl|2xl)/,
+        }
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+    ],
 }
 
