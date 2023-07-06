4135cd0c01f3e1a46bc1f63df4e0c47b461c8b26
hadjidoro <>
2023-07-06 18:39:17 +0000

    set config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@ export const environment = {
         baseUrl: "https://engine-back-end-dev-dot-credit-digital-386211.oa.r.appspot.com/api/v1"
     },
     keycloak: {
-        baseUrl: "https://dev-sso.gutouch.net/auth",
-        realm: "creditdigital",
+        baseUrl: "https://sso.gutouch.net/auth",
+        realm: "credit-digital",
         clientId: "engine",
         redirectUri: "https://engine-front-end-dev-dot-credit-digital-386211.oa.r.appspot.com"
     }
