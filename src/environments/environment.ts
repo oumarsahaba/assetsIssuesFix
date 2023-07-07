443f1fed56a04fd3b019379a1ff009ebbc0b65be
hadjidoro <>
2023-07-07 12:48:15 +0000

    use substitition value to set env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: true,
     engine: {
-        baseUrl: "/%ENGINE_BASE_URL%/"
+        baseUrl: "%ENGINE_BASE_URL%"
     },
     keycloak: {
-        baseUrl: "/%KEYCLOAK_SERVER_URL%/",
-        realm: "/%KEYCLOAK_REALM%/",
-        clientId: "/%KEYCLOAK_RESOURCE%/",
-        redirectUri: "/%KEYCLOAK_REDIRECT_URL%/"
+        baseUrl: "%KEYCLOAK_SERVER_URL%",
+        realm: "%KEYCLOAK_REALM%",
+        clientId: "%KEYCLOAK_RESOURCE%",
+        redirectUri: "%KEYCLOAK_REDIRECT_URL%"
     }
 };
