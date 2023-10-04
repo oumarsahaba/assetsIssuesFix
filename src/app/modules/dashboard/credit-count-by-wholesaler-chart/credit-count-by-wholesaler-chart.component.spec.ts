80e6bc0685eb2c27af5512df8a6df7223e149d29
Oumar Sahaba NDIAYE <oumarsahaba.ndiaye@intouchgroup.net>
2023-10-04 10:40:03 +0000

    feature: add credit flux and count by wholesaler charts and replace credit by agent charts by them

---
+++ src/app/modules/dashboard/credit-count-by-wholesaler-chart/credit-count-by-wholesaler-chart.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CreditCountByWholesalerChartComponent } from './credit-count-by-wholesaler-chart.component';
+
+describe('CreditCountByWholesalerChartComponent', () => {
+  let component: CreditCountByWholesalerChartComponent;
+  let fixture: ComponentFixture<CreditCountByWholesalerChartComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CreditCountByWholesalerChartComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CreditCountByWholesalerChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/dashboard/credit-flux-by-wholesaler-chart/credit-flux-by-wholesaler-chart.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CreditFluxByWholesalerChartComponent } from './credit-flux-by-wholesaler-chart.component';
+
+describe('CreditFluxByWholesalerChartComponent', () => {
+  let component: CreditFluxByWholesalerChartComponent;
+  let fixture: ComponentFixture<CreditFluxByWholesalerChartComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CreditFluxByWholesalerChartComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CreditFluxByWholesalerChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
