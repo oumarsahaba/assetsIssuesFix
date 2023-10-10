6070fa1c2e1f117706870bd6757ab9426bd6a15f
Oumar Sahaba NDIAYE <oumarsahaba.ndiaye@intouchgroup.net>
2023-10-10 09:22:08 +0000

    config : override console.log function in production mode in order to return nothing

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 import {AppModule} from './app/app.module';
+import { environment } from './environments/environment';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.error(err));
+
+// Disable logs in production
+if(environment.production){
+    window.console.log = () => {}
+}
