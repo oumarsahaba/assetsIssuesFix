87bfbc8b7f686eb8c6d48294e74cd87fea62e550
hadjidoro <dorogueye@gmail.com>
2023-10-11 13:40:39 +0000

    ui refactoring

--- src/app/modules/resources/agent/import-excel/import-excel.component.ts
+++ src/app/modules/resources/agent/import-excel/import-excel.component.ts
@@ -1,111 +1,101 @@
-import { HttpEventType } from '@angular/common/http';
-import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { ToastrService } from 'ngx-toastr';
-import { Observable, of, timeout } from 'rxjs';
-import { AppError } from 'src/app/commons/errors/app-error';
-import { BadRequestError } from 'src/app/commons/errors/bad-request-error';
-import { UnprocessableEntityError } from 'src/app/commons/errors/unprocessable-entity-error';
-import { handleFormError } from 'src/app/commons/helpers';
-import { AgentFromExcel } from 'src/app/commons/interfaces/agent-from-excel';
-import { AgentService } from 'src/app/services/agent.service';
+import {HttpEventType} from '@angular/common/http';
+import {Component, OnInit, OnDestroy, ViewChild, ElementRef} from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
+import {ToastrService} from 'ngx-toastr';
+import {Observable, of, timeout} from 'rxjs';
+import {AppError} from 'src/app/commons/errors/app-error';
+import {BadRequestError} from 'src/app/commons/errors/bad-request-error';
+import {UnprocessableEntityError} from 'src/app/commons/errors/unprocessable-entity-error';
+import {handleFormError} from 'src/app/commons/helpers';
+import {AgentFromExcel} from 'src/app/commons/interfaces/agent-from-excel';
+import {AgentService} from 'src/app/services/agent.service';
 import * as XLSX from 'xlsx';
 
 
 @Component({
-  selector: 'app-import-excel',
-  templateUrl: './import-excel.component.html',
-  styleUrls: ['./import-excel.component.css']
+    selector: 'app-import-excel',
+    templateUrl: './import-excel.component.html',
+    styleUrls: ['./import-excel.component.css']
 })
-export class ImportExcelComponent implements OnInit, OnDestroy{
-  
-  @ViewChild('inputFile')
-  inputFile: ElementRef;
-  displayModal: any = false;
-  file: File;
-  agents: AgentFromExcel[]
-  sheetName: string
-  agents$: Observable<AgentFromExcel[]>
-  uploadProgression = false
-  uploadCompleted = false
-  form = new FormGroup({
-    file: new FormControl('', Validators.required),
-})
-  
+export class ImportExcelComponent {
+
+    @ViewChild('inputFile')
+    inputFile: ElementRef;
+    displayModal: any = false;
+    file: File;
+    agents: AgentFromExcel[]
 
 
-  constructor( private agentService: AgentService,
-    private toastr: ToastrService){}
+    uploadProgression = false
+    uploadCompleted = false
 
-
-  ngOnInit(): void {
-  }
-
-  ngOnDestroy(){
-  }
-  
-  chooseFile(){
-    this.inputFile.nativeElement.click()
-  }
-  reloadupload(){
-    //@ts-ignore
-    this.file= undefined
-    this.uploadCompleted = false
-    this.uploadProgression = false
-    this.form.reset()
-    this.inputFile.nativeElement.value = ""     
-    console.log(this.file);
-    
-  
-  }
-  importFile(){    
-
-    if (this.form.valid) {
-      
-      this.uploadProgression = true
-      this.uploadCompleted = false
-      this.agentService.uploadAgentsFromExcel(this.file).subscribe(
-        {next: event => {
-          console.log(event);
-          
-        
-        if (event.type === HttpEventType.Response) {
-          this.uploadCompleted = true
-          this.uploadProgression = false
-          this.toggleModal()
-          this.toastr.success('File successfully uploaded');
-        }
-      
-        },
-        error: (err: AppError) => {
-          if (err instanceof UnprocessableEntityError)
-            handleFormError(err, this.form)  
-          setTimeout(() => {
-            this.uploadProgression = false
-          }, 2000);    
-          
-
-        }
-        
+    form = new FormGroup({
+        file: new FormControl('', Validators.required),
     })
+
+    constructor(private agentService: AgentService,
+                private toastr: ToastrService) {
     }
-    
-    
-  }
-  // On file Select 
-  onChange(event : any) { 
-    this.file = event.target.files[0]; 
-    this.form.get('file')?.setValue(this.file.name)
-    console.log(this.file, event.type);
-    
-}
 
-  toggleModal() {
-    this.displayModal = !this.displayModal
-    this.reloadupload()
-}
+    chooseFile() {
+        this.inputFile.nativeElement.click()
+    }
+
+    reloadUpload() {
+        //@ts-ignore
+        this.file = undefined
+        this.uploadCompleted = false
+        this.uploadProgression = false
+        this.form.reset()
+        this.inputFile.nativeElement.value = ""
+        console.log(this.file);
+    }
+
+    importFile() {
+        if (this.form.valid) {
+
+            this.uploadProgression = true
+            this.uploadCompleted = false
+            this.agentService.uploadAgentsFromExcel(this.file).subscribe(
+                {
+                    next: event => {
+                        console.log(event);
 
 
+                        if (event.type === HttpEventType.Response) {
+                            this.uploadCompleted = true
+                            this.uploadProgression = false
+                            this.toggleModal()
+                            this.toastr.success('File successfully uploaded');
+                        }
+
+                    },
+                    error: (err: AppError) => {
+                        if (err instanceof UnprocessableEntityError)
+                            handleFormError(err, this.form)
+                        setTimeout(() => {
+                            this.uploadProgression = false
+                        }, 2000);
+
+
+                    }
+
+                })
+        }
+        
+    }
+
+    // On file Select
+    onChange(event: any) {
+        this.file = event.target.files[0];
+        this.form.get('file')?.setValue(this.file.name)
+        console.log(this.file, event.type);
+    }
+
+    toggleModal() {
+        this.displayModal = !this.displayModal
+        this.reloadUpload()
+    }
 
 }
 
