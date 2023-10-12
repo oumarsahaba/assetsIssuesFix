b4a40b15b442744e4af6ee27ffa6412bedb3fa91
hadjidoro <dorogueye@gmail.com>
2023-10-12 12:17:38 +0000

    fix import issue caused by merge

--- src/app/modules/resources/agent/import-excel/import-excel.component.ts
+++ src/app/modules/resources/agent/import-excel/import-excel.component.ts
@@ -1,104 +1,28 @@
 import {HttpEventType} from '@angular/common/http';
-import {Component, OnInit, OnDestroy, ViewChild, ElementRef} from '@angular/core';
-import {FormGroup, FormControl, Validators} from '@angular/forms';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {ToastrService} from 'ngx-toastr';
-import {Observable, of, timeout} from 'rxjs';
 import {AppError} from 'src/app/commons/errors/app-error';
-import {BadRequestError} from 'src/app/commons/errors/bad-request-error';
 import {UnprocessableEntityError} from 'src/app/commons/errors/unprocessable-entity-error';
 import {handleFormError} from 'src/app/commons/helpers';
 import {AgentFromExcel} from 'src/app/commons/interfaces/agent-from-excel';
 import {AgentService} from 'src/app/services/agent.service';
-import * as XLSX from 'xlsx';
-
 
 @Component({
     selector: 'app-import-excel',
     templateUrl: './import-excel.component.html',
     styleUrls: ['./import-excel.component.css']
 })
-export class ImportExcelComponent implements OnInit, OnDestroy{
-  
-  @ViewChild('inputFile')
-  inputFile: ElementRef;
-  displayModal: any = false;
-  file: File;
-  agents: AgentFromExcel[]
-  sheetName: string
-  agents$: Observable<AgentFromExcel[]>
-  uploadProgression = false
-  uploadCompleted = false
-  templateDownloaded = false
-  form = new FormGroup({
-    file: new FormControl('', Validators.required),
-})
-  
-
-
-  constructor( private agentService: AgentService,
-    private toastr: ToastrService){}
-
-
-  ngOnInit(): void {
-  }
-
-  ngOnDestroy(){
-
-  }
-  
-  chooseFile(){
-    this.inputFile.nativeElement.click()
-  }
-  reloadupload(){
-    //@ts-ignore
-    this.file= undefined
-    this.uploadCompleted = false
-    this.uploadProgression = false
-    this.form.reset()
-    this.inputFile.nativeElement.value = ""     
-    console.log(this.file);
-    
-  
-  }
-  importFile(){    
-
-    if (this.form.valid) {
-      
-      this.uploadProgression = true
-      this.uploadCompleted = false
-      this.agentService.uploadAgentsFromExcel(this.file).subscribe(
-        {next: event => {
-          console.log(event);
-          
-        
-        if (event.type === HttpEventType.Response) {
-          this.uploadCompleted = true
-          this.uploadProgression = false
-          this.templateDownloaded = false
-          this.toggleModal()
-          this.toastr.success('File successfully uploaded');
-        }
-      
-        },
-        error: (err: AppError) => {
-          if (err instanceof UnprocessableEntityError)
-            handleFormError(err, this.form)  
-          setTimeout(() => {
-            this.uploadProgression = false
-            this.templateDownloaded = false
-          }, 2000);    
-          
+export class ImportExcelComponent {
 
     @ViewChild('inputFile')
     inputFile: ElementRef;
     displayModal: any = false;
     file: File;
     agents: AgentFromExcel[]
-
-
     uploadProgression = false
     uploadCompleted = false
-
+    templateDownloaded = false
     form = new FormGroup({
         file: new FormControl('', Validators.required),
     })
@@ -111,7 +35,7 @@ export class ImportExcelComponent implements OnInit, OnDestroy{
         this.inputFile.nativeElement.click()
     }
 
-    reloadUpload() {
+    refreshUpload() {
         //@ts-ignore
         this.file = undefined
         this.uploadCompleted = false
@@ -126,33 +50,31 @@ export class ImportExcelComponent implements OnInit, OnDestroy{
 
             this.uploadProgression = true
             this.uploadCompleted = false
-            this.agentService.uploadAgentsFromExcel(this.file).subscribe(
-                {
-                    next: event => {
-                        console.log(event);
+            this.agentService.uploadAgentsFromExcel(this.file).subscribe({
+                next: event => {
+                    console.log(event);
 
 
-                        if (event.type === HttpEventType.Response) {
-                            this.uploadCompleted = true
-                            this.uploadProgression = false
-                            this.toggleModal()
-                            this.toastr.success('File successfully uploaded');
-                        }
-
-                    },
-                    error: (err: AppError) => {
-                        if (err instanceof UnprocessableEntityError)
-                            handleFormError(err, this.form)
-                        setTimeout(() => {
-                            this.uploadProgression = false
-                        }, 2000);
-
-
+                    if (event.type === HttpEventType.Response) {
+                        this.uploadCompleted = true
+                        this.uploadProgression = false
+                        this.templateDownloaded = false
+                        this.toggleModal()
+                        this.toastr.success('File successfully uploaded');
                     }
 
-                })
+                },
+
+                error: (err: AppError) => {
+                    if (err instanceof UnprocessableEntityError)
+                        handleFormError(err, this.form)
+                    setTimeout(() => {
+                        this.uploadProgression = false
+                        this.templateDownloaded = false
+                    }, 2000);
+                }
+            });
         }
-        
     }
 
     // On file Select
@@ -164,7 +86,7 @@ export class ImportExcelComponent implements OnInit, OnDestroy{
 
     toggleModal() {
         this.displayModal = !this.displayModal
-        this.reloadUpload()
+        this.refreshUpload()
     }
 
 }
